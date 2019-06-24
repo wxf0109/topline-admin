38c30348b282ef8cbc7e651b91da56845d8ff116
wangxuefeng <xf200109@163.com>
2019-06-24 20:50:47 +0800

    登录功能完成

---
+++ src/views/layout/components/Appbian.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p></p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,7 +18,12 @@
               <el-input v-model="form.code" placeholder="请输入验证码"></el-input>
             </el-col>
             <el-col :span="10" :offset="2">
-              <el-button @click="handleCode">获取验证码</el-button>
+              <el-button
+              @click="handleCode"
+              :disabled="!!codeTimer || codeLoadging"
+              >
+                {{codeTimer ? `剩余${codeSecons}秒` : '获取验证码'}}
+              </el-button>
             </el-col>
           </el-form-item>
           <el-form-item prop="checked">
@@ -39,17 +44,19 @@ import axios from 'axios'
 
 import '@/vendor/gt'
 
+const djs = 60
+
 export default {
   name: 'AppLogin',
   data () {
     return {
-      form: {
+      form: {// 表单数据
         mobile: '15076653737',
         code: '',
-        checked: true
+        checked: true// 是否同意用户协议
       },
-      loginLoading: false,
-      rules: {
+      loginLoading: false, // 登录按钮的 loading状态
+      rules: { // 表单验证规则
         mobile: [
           { required: true, message: '请输入手机号', trigger: 'blur' },
           { min: 11, max: 11, message: '长度必须为11个字符', trigger: 'blur' }
@@ -63,7 +70,11 @@ export default {
           { pattern: /true/, message: '请同意用户协议', trigger: 'change' }
         ]
       },
-      captchaObj: null
+      captchaObj: null, // 通过 initGeetest 得到的极验验证码对象
+      codeSecons: djs, // 倒计时的时间
+      codeTimer: null, // 倒计时定时器
+      sendMobie: '', // 保存初始化验证码之后发送短息的手机号
+      codeLoadging: false
     }
   },
   methods: {
@@ -74,7 +85,6 @@ export default {
         if (!valid) {
           return
         }
-
         this.login()
       })
     },
@@ -107,15 +117,42 @@ export default {
       })
     },
     handleCode () {
-      const { mobile } = this.form
+      this.$refs['ruleForm'].validateField('mobile', errorMessage => {
+        // 判断  如果有错误  return 不往后执行
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        // 手机号码验证通过
+        // 验证是否有验证码插件对象
+        if (this.captchaObj) {
+          // 验证通过  初始化验证 码
+        // this.showGeetest()
+        // 如果用户输入 的手机号和之前初始化的验证码手机号不一致，就基于当前手机号重新初始化
+        // 否则，直接 verify 显示
+          if (this.form.mobile !== this.sendMobile) {
+          // 重新初始化验证码插件
 
-      if (this.captchaObj) {
-        return this.captchaObj.verify()
-      }
+            // 重新初始化之前，将原来的验证码插件删除
+            document.body.removeChild(document.querySelector('.geetest_panel'))
+            this.showGeetest()
+          } else {
+            this.captchaObj.verify()
+          }
+        } else {
+        // 这是第一次的初始化验证码
+          this.showGeetest()
+        }
+      })
+    },
+    showGeetest () {
+      // const { mobile } = this.form
+      // 函数中的 function 函数中的 this 指向window
+      // 初始化验证码时禁用按钮
+      this.codeLoadging = true
 
       axios({
         method: 'GET',
-        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${this.form.mobile}`
       }).then(res => {
         const data = res.data.data
         window.initGeetest(
@@ -125,11 +162,17 @@ export default {
             challenge: data.challenge,
             offline: !data.success,
             new_captcha: data.new_captcha,
-            product: 'bind'
+            product: 'bind' // 隐藏按钮式
           }, (captchaObj) => {
             this.captchaObj = captchaObj
+            // 这里可以调用验证实例 captchaObj 的实例方法
             captchaObj.onReady(() => {
+              // 只有 ready 了才能显示验证码
+              this.sendMobile = this.form.mobile
               captchaObj.verify()
+
+              // 验证码初始化完成后，解除禁用发送验证码按钮
+              this.codeLoadging = false
             }).onSuccess(() => {
               console.dir(captchaObj.getValidate())
               const {
@@ -139,22 +182,31 @@ export default {
               } = captchaObj.getValidate()
               axios({
                 method: 'GET',
-                url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+                url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${this.form.mobile}`,
                 params: {
                   challenge,
                   validate,
                   seccode
                 }
               }).then(res => {
-                console.log(res.data)
-              }).catch(error => {
-                console.log(error)
+                // 发送倒计时
+                this.codeCountDown()
               })
             })
             // 这里可以调用验证实例 captchaObj 的实例方法
           }
         )
       })
+    },
+    codeCountDown () {
+      this.codeTimer = window.setInterval(() => {
+        this.codeSecons--
+        if (this.codeSecons <= 0) {
+          this.codeSecons = djs
+          window.clearInterval(this.codeTimer)
+          this.codeTimer = null
+        }
+      }, 1000)
     }
   }
 }
